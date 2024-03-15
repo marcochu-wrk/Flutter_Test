42dd1eb8969ccd649bf3ac169b6e8fd0f7374230
marcochu-wrk <119313395+marcochu-wrk@users.noreply.github.com>
2024-03-15 08:51:32 +0200

    Separating classes into different files

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
+import 'myHomePage.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class Post {
-  String body;
-  String author;
-  int likes = 0;
-  bool userLiked = false;
-
-  Post(this.body, this.author);
-
-  void likePost() {
-    this.userLiked = !this.userLiked;
-    if (this.userLiked) {
-      this.likes++;
-    } else {
-      this.likes -= 1;
-    }
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -38,113 +21,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<Post> posts = [];
 
-  void newPost(String text) {
-    this.setState(() {
-      posts.add(new Post(text, "Auth"));
-    });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Hello World')),
-        body: Column(children: <Widget>[
-          Expanded(child: PostList(this.posts)),
-          TextInputWidget(this.newPost)
-        ]));
-  }
-}
-
-class TextInputWidget extends StatefulWidget {
-  final Function(String) callback;
-
-  TextInputWidget(this.callback);
-
-  @override
-  State<TextInputWidget> createState() => _TextInputWidgetState();
-}
-
-class _TextInputWidgetState extends State<TextInputWidget> {
-  final controller = TextEditingController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
-  }
-
-  void click() {
-    FocusScope.of(context).unfocus();
-    widget.callback(controller.text);
-    controller.clear();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-        controller: this.controller,
-        decoration: InputDecoration(
-            prefixIcon: Icon(Icons.message),
-            labelText: "Type a message:",
-            suffixIcon: IconButton(
-                icon: Icon(Icons.send),
-                splashColor: Colors.orange,
-                tooltip: "Post Message",
-                onPressed: this.click)));
-  }
-}
-
-class PostList extends StatefulWidget {
-  final List<Post> listItems;
-  PostList(this.listItems); //Constructor
-
-  @override
-  State<PostList> createState() => _PostListState();
-}
-
-class _PostListState extends State<PostList> {
-  void like(Function callBack) {
-    this.setState(() {
-      callBack();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: this.widget.listItems.length,
-      itemBuilder: (context, index) {
-        var post = this.widget.listItems[index];
-        return Card(
-            child: Row(children: <Widget>[
-          Expanded(
-              child: ListTile(
-            title: Text(post.body),
-            subtitle: Text(post.author),
-          )),
-          Row(children: <Widget>[
-            Container(
-                child:
-                    Text(post.likes.toString(), style: TextStyle(fontSize: 20)),
-                padding: EdgeInsets.fromLTRB(0, 0, 10, 0)),
-            IconButton(
-              icon: Icon(Icons.thumb_up),
-              onPressed: () => like(post.likePost),
-              color: post.userLiked ? Colors.red : Colors.black
-            )
-          ])
-        ]));
-      },
-    );
-  }
-}
---
+++ flutter_application_1/lib/myHomePage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'post.dart';
+import 'postList.dart';
+import 'textInputWidget.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Post> posts = [];
+
+  void newPost(String text) {
+    this.setState(() {
+      posts.add(new Post(text, "Auth"));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Hello World')),
+        body: Column(children: <Widget>[
+          Expanded(child: PostList(this.posts)),
+          TextInputWidget(this.newPost)
+        ]));
+  }
+}
